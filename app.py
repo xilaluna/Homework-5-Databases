0de785c014ff661aa83f1fd26c91242624ad16b2
xiluna <xilaluna2@gmail.com>
2020-11-30 22:53:23 -0800

    done

--- app.py
+++ app.py
@@ -10,10 +10,12 @@ app = Flask(__name__)
 
 app.config["MONGO_URI"] = "mongodb://localhost:27017/plantsDatabase"
 mongo = PyMongo(app)
-
+plants_collection = mongo.db.plants
+harvest_collection = mongo.db.harvests
 ############################################################
 # ROUTES
 ############################################################
+
 
 @app.route('/')
 def plants_list():
@@ -21,38 +23,47 @@ def plants_list():
 
     # TODO: Replace the following line with a database call to retrieve *all*
     # plants from the Mongo database's `plants` collection.
-    plants_data = ''
+    plants_data = plants_collection.find()
 
     context = {
         'plants': plants_data,
     }
     return render_template('plants_list.html', **context)
 
+
 @app.route('/about')
 def about():
     """Display the about page."""
     return render_template('about.html')
 
+
 @app.route('/create', methods=['GET', 'POST'])
 def create():
     """Display the plant creation page & process data from the creation form."""
     if request.method == 'POST':
-        # TODO: Get the new plant's name, variety, photo, & date planted, and 
+        # TODO: Get the new plant's name, variety, photo, & date planted, and
         # store them in the object below.
+        users_plant_name = request.form.get('plant_name')
+        users_plants_variety = request.form.get('variety')
+        users_plants_photo_url = request.form.get('photo')
+        users_plant_date_planted = request.form.get('date_planted')
+
         new_plant = {
-            'name': '',
-            'variety': '',
-            'photo_url': '',
-            'date_planted': ''
+            'name': users_plant_name,
+            'variety': users_plants_variety,
+            'photo_url': users_plants_photo_url,
+            'date_planted': users_plant_date_planted
         }
         # TODO: Make an `insert_one` database call to insert the object into the
-        # database's `plants` collection, and get its inserted id. Pass the 
+        # database's `plants` collection, and get its inserted id. Pass the
         # inserted id into the redirect call below.
-
-        return redirect(url_for('detail', plant_id=''))
+        users_plant = plants_collection.insert_one(new_plant)
+        inserted_users_plant = users_plant.inserted_id
+        return redirect(url_for('detail', plant_id=inserted_users_plant))
 
     else:
         return render_template('create.html')
+
 
 @app.route('/plant/<plant_id>')
 def detail(plant_id):
@@ -60,19 +71,21 @@ def detail(plant_id):
 
     # TODO: Replace the following line with a database call to retrieve *one*
     # plant from the database, whose id matches the id passed in via the URL.
-    plant_to_show = ''
+    plant_to_show = plants_collection.find_one(
+        {'_id': ObjectID(plant_id)})
 
     # TODO: Use the `find` database operation to find all harvests for the
     # plant's id.
     # HINT: This query should be on the `harvests` collection, not the `plants`
     # collection.
-    harvests = ''
+    harvests = harvest_collection.find({'plant_id': plant_id})
 
     context = {
-        'plant' : plant_to_show,
+        'plant': plant_to_show,
         'harvests': harvests
     }
     return render_template('detail.html', **context)
+
 
 @app.route('/harvest/<plant_id>', methods=['POST'])
 def harvest(plant_id):
@@ -82,16 +95,20 @@ def harvest(plant_id):
 
     # TODO: Create a new harvest object by passing in the form data from the
     # detail page form.
+    quantity = request.form.get('harvested_amount')
+    date = request.form.get('date_planted')
+
     new_harvest = {
-        'quantity': '', # e.g. '3 tomatoes'
-        'date': '',
+        'quantity': quantity,  # e.g. '3 tomatoes'
+        'date': date,
         'plant_id': plant_id
     }
 
-    # TODO: Make an `insert_one` database call to insert the object into the 
+    # TODO: Make an `insert_one` database call to insert the object into the
     # `harvests` collection of the database.
-
+    harvest_collection.insert_one(new_harvest)
     return redirect(url_for('detail', plant_id=plant_id))
+
 
 @app.route('/edit/<plant_id>', methods=['GET', 'POST'])
 def edit(plant_id):
@@ -99,13 +116,23 @@ def edit(plant_id):
     if request.method == 'POST':
         # TODO: Make an `update_one` database call to update the plant with the
         # given id. Make sure to put the updated fields in the `$set` object.
+        edit_name = request.form.get('plant_name')
+        edit_variety = request.form.get('variety')
+        edit_photo = request.form.get('photo')
+        edit_date_planted = request.form.get('date_planted')
 
-        
+        plants_collection.update_one({'_id': f'ObjectID({plant_id})'},
+                                     {'$set': {
+                                         'name': edit_name,
+                                         'variety': edit_variety,
+                                         'photo_url': edit_photo,
+                                         'date_planted': edit_date_planted}})
         return redirect(url_for('detail', plant_id=plant_id))
     else:
         # TODO: Make a `find_one` database call to get the plant object with the
         # passed-in _id.
-        plant_to_show = ''
+        plant_to_show = plants_collection.find_one(
+            {'_id': ObjectID(plant_id)})
 
         context = {
             'plant': plant_to_show
@@ -113,16 +140,18 @@ def edit(plant_id):
 
         return render_template('edit.html', **context)
 
+
 @app.route('/delete/<plant_id>', methods=['POST'])
 def delete(plant_id):
     # TODO: Make a `delete_one` database call to delete the plant with the given
     # id.
-
+    plants_collection.delete_one({'_id': ObjectID(plant_id)})
     # TODO: Also, make a `delete_many` database call to delete all harvests with
     # the given plant id.
+    harvest_collection.delete_many({'plant_id': plant_id})
 
     return redirect(url_for('plants_list'))
 
+
 if __name__ == '__main__':
     app.run(debug=True)
-
